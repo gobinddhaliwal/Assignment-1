9d7457bc240739e1fdac9ffbc68d84252e5b72fa
gobinddhaliwal <dhaliwalgobind@gmail.com>
2019-02-16 22:21:37 -0500

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -10,19 +10,23 @@ router.get('/aboutme', (req, res, next) => {
   res.render('aboutme/index', { title: 'About Me' });
 });
 
-
-
 router.get('/contact', (req, res, next) => {
   res.render('index', { title: 'Contact' });
 });
-
+//Configured Route for projects
 router.get('/projects', (req, res, next) => {
   res.render('projects/index', { title: 'Projects' });
 });
 router.get('/projects/details-1', (req, res, next) => {
   res.render('projects/project1', { title: 'Projects' });
 });
-
+router.get('/projects/details-2', (req, res, next) => {
+  res.render('projects/project2', { title: 'Projects' });
+});
+router.get('/projects/details-3', (req, res, next) => {
+  res.render('projects/project3', { title: 'Projects' });
+});
+// configured route for services
 router.get('/services', (req, res, next) => {
   res.render('services/index', { title: 'Services' });
 });
